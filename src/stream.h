d3cc5c2d55ba59541682a2fb718edcf5ec1885c7
malav-mehta <malavhmehta@outlook.com>
2021-05-31 10:52:10 -0400

    Added stream.h

---
+++ src/stream.h
@@ -0,0 +1,41 @@
+#ifndef STREAM_H_INCLUDED
+#define STREAM_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+/**
+ * A singly linked list implementation for the peekable stream used
+ * to read characters from the file.
+ */
+template <typename T>
+struct Node { 
+  T val;
+  Node *next;
+
+  Node();
+  Node(T _val);
+  Node(T _val, Node* _next);
+};
+
+/*
+ * A readable stream implementing the linked lits defined above to
+ * read file characters and peek forward. 
+ */
+class Stream {
+ private:
+  Node<char> char_stream;
+  std::string file_name;
+
+ public:
+  Stream();
+  Stream(std::string file_name);
+  void SetFileName();
+  void Initialize();
+  char Next() const;
+  void MoveNext();
+  ~Stream();
+};
+
+#endif // #ifndef STREAM_H_INCLUDED
